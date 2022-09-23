56026dd25babc5bc225c0cf8d372ed619bb1ca31
hirwaaldo1 <hirwaaldo1@gmail.com>
2022-09-23 09:38:05 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundColor:{
+        appleHeader:"rgba(0, 0, 0, 1)"
+      },
+      fontFamily:{
+        sans:"apple"
+      }
+    },
+  },
+  plugins: [],
+}
